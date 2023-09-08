27d0f734e23ead5f576d71c91ec0b78c7f06a4c1
GitHub Action <action@github.com>
2023-09-08 12:56:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/alvinli1991/metatoolkitplugin/MyBundle.kt
+++ src/main/kotlin/com/github/alvinli1991/metatoolkitplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alvinli1991.metatoolkitplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alvinli1991/metatoolkitplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/alvinli1991/metatoolkitplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alvinli1991.metatoolkitplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
